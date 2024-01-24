1e57ac2a7f600e756a0d518cc174e04db420757b
AgIgor <agigor@outlook.com>
2024-01-24 19:54:03 -0300

    mqtt callback

--- Projects/Esp32/Esp32_MQTT/src/main.cpp
+++ Projects/Esp32/Esp32_MQTT/src/main.cpp
@@ -14,60 +14,10 @@ PubSubClient client(espClient);
 
 const char* subscriberTopic = "@igor_db";
 const char* publisherTopic = "@igor_esp32";
-bool led;
-
-void callback(char* topic, byte* payload, unsigned int length);
-void reconnect();
-void publishMessage();
-
-void setup() {
-  Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-  pinMode(0, INPUT_PULLUP);
-
-  // Conecte-se à rede WiFi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Conectando ao WiFi...");
-  }
-  Serial.println("Conectado ao WiFi");
-
-  // Configurar o servidor MQTT
-  client.setServer(mqtt_server, mqtt_port);
-  client.setCallback(callback);
-
-  // Conectar ao servidor MQTT
-  while (!client.connected()) {
-    Serial.println("Conectando ao servidor MQTT...");
-    if (client.connect("ESP32Client")) {
-      Serial.println("Conectado ao servidor MQTT");
-
-      // Inscrever-se no tópico do subscriber
-      client.subscribe(subscriberTopic);
-      //client.subscribe(publisherTopic);//
-
-    } else {
-      Serial.print("Falha, rc=");
-      Serial.print(client.state());
-      Serial.println(" Tentando novamente em 5 segundos");
-      delay(5000);
-    }
-  }
-  digitalWrite(LED_BUILTIN, LOW);
-}//end setup
-
-void loop() {
-  // Manter a conexão com o servidor MQTT
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
-  publishMessage();
-}//end loop
+bool led = false;
 
 void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.println();
   Serial.print("Mensagem recebida no tópico: ");
   Serial.println(topic);
 
@@ -77,27 +27,31 @@ void callback(char* topic, byte* payload, unsigned int length) {
     data += (char)payload[i];
     //Serial.print(data);
   }
+  
   Serial.println(data);
+  Serial.println();
 
   JsonDocument jsonPayload;
   deserializeJson(jsonPayload, data);
   int sensor1 = jsonPayload["Sensor1"]["Temp"];
   int sensor2 = jsonPayload["Sensor2"]["Humidity"];
   bool sensor3 = jsonPayload["Sensor3"]["Light"];
+  String by = jsonPayload["by"];
 
   Serial.println("Json");
   Serial.println(sensor1);
   Serial.println(sensor2);
   Serial.println(sensor3);
+  Serial.println(by);
   Serial.println();
 
-  if(sensor3){
-    digitalWrite(LED_BUILTIN, HIGH);
+  if(sensor3 and by != "null"){
     led = true;
+    client.publish(publisherTopic, "{\"Sensor3\": {\"Light\": true}}");
   }
-  if(!sensor3){
-    digitalWrite(LED_BUILTIN, LOW);
+  else{
     led = false;
+    client.publish(publisherTopic, "{\"Sensor3\": {\"Light\": false}}");
   }
 }
 
@@ -125,13 +79,61 @@ void publishMessage() {
     delay(500);
     led = !led;
     if(led){
-      //client.publish(publisherTopic, "{\"Sensor3\": {\"Light\": true}}");
-      client.publish(subscriberTopic, "{\"Sensor3\": {\"Light\": true}}");
+      client.publish(publisherTopic, "{\"Sensor3\": {\"Light\": true}}");
+      Serial.println("Btn Light: true");  
     }
     else {
-      //client.publish(publisherTopic, "{\"Sensor3\": {\"Light\": false}}");
-      client.publish(subscriberTopic, "{\"Sensor3\": {\"Light\": false}}");
+      client.publish(publisherTopic, "{\"Sensor3\": {\"Light\": false}}");
+      Serial.println("Btn Light: false");  
     }
-    Serial.println("send");
   }
+  digitalWrite(LED_BUILTIN, led);
 }
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  pinMode(0, INPUT_PULLUP);
+
+  // client.setCallback(callback);
+
+  // Conecte-se à rede WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Conectando ao WiFi...");
+  }
+  Serial.println("Conectado ao WiFi");
+
+  // Configurar o servidor MQTT
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+
+  // Conectar ao servidor MQTT
+  while (!client.connected()) {
+    Serial.println("Conectando ao servidor MQTT...");
+    if (client.connect("ESP32Client")) {
+      Serial.println("Conectado ao servidor MQTT");
+      // Inscrever-se no tópico do subscriber
+      client.subscribe(subscriberTopic);
+
+    } else {
+      Serial.print("Falha, rc=");
+      Serial.print(client.state());
+      Serial.println(" Tentando novamente em 5 segundos");
+      delay(5000);
+    }
+  }
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(100);
+}//end setup
+
+void loop() {
+  // Manter a conexão com o servidor MQTT
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  publishMessage();
+}//end loop
